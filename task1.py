b07222cca4010f26678de5c0343da474acd0fa77
Агафонова Анна <annaagafonova03@mail.ru>
2025-07-03 16:57:06 +0700

    Задание 1 сделано

---
+++ task1.py
@@ -0,0 +1,77 @@
+# Задание 1
+
+# example_str = '***---Добро пожаловать!---***'
+# result = example_str.strip('***---')
+# print(result)
+
+# Задание 2
+
+# example_str = 'В строке, есть, ошибка, снова ошибка.'
+# a=example_str.replace(',', '.')
+# a1=a.replace('ошибка', 'исправление')
+# print(a1)
+
+# Задание 3
+
+# example_str = 'Иванов;Петров;Сидоров;Кузнецов'
+# b=example_str.split(';')
+# b1=list(b)
+# b2=b1[1]
+# print(b2)
+
+# Задание 4
+
+# words = ['Скоро', 'будет', 'контрольная']
+# c=' '.join(words) + '.'
+# print(c)
+
+# Задание 5
+
+# nums = [4, 2, 9, 0, 4, 7]
+# nums.remove(4)
+# nums.sort()
+# print(nums)
+
+# Задание 6
+
+# numbers = [10, 20, 30, 40]
+# numbers.pop(-1)
+# print(numbers)
+
+# Задание 7
+
+# numbers = [5, 2, 9, 1]
+# numbers.sort()
+# print(numbers)
+
+# Задание 8
+
+# nums = [3, 1, 3, 7, 3, 2]
+# result=nums.count(3)
+# print(result)
+
+# Задание 9
+
+# d=('RGB', 255, 0, 128)
+# mode, r, g, b = d
+# print(mode)
+# print(r)
+# print(g)
+# print(b)
+
+# Задание 10
+
+# dictionary={'a': 1, 'b': 2}
+# dictionary['c']=3
+# dictionary['a']=1+5
+# print(dictionary)
+
+# Задание 11
+
+# person = {'имя': 'Аня', 'город': 'Тверь'}
+# print(person['имя'])
+
+# Задание 12
+
+# person = {'имя': 'Игорь'}
+# print(person.get('Номер телефона', 'Неизвестно'))
