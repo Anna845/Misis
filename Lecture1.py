a4c064b289f5bf220a49ce05592f5da38ce0f379
Агафонова Анна <annaagafonova03@mail.ru>
2025-07-04 17:32:16 +0700

    Лекция 1

---
+++ Lecture1.py
@@ -0,0 +1,42 @@
+# Задание 0
+
+# a='Кабачки полезны для здоровья.'
+# print(a[0]+a[10]+a[1]+a[-13])
+# print(a[0])
+# print(a[10])
+# print(a[1])
+# print(a[-13])
+
+# Задание 1
+
+# example_str='Snake_Case'
+# result0=example_str.replace('_', '')
+# result=result0.replace('Snake', 'Camel')
+# print(result)
+
+# b='max_value_of_array'
+# b1=b.replace('_', ' ')
+# b2=str.title(b1)
+# b3=str.split(b2)
+# b4=str.lower(b3[0]) + b3[1] + b3[2] + b3[3]
+# print(b4)
+
+# Задание 2
+
+# numbers=list('0123455678921')
+# first_index = numbers.index('1') 
+# second_index = numbers.index('1', first_index + 1) 
+# c=numbers.index('2')
+# c1=numbers.index('2', c + 1)
+# d=numbers.index('5')
+# d1=numbers.index('5', d + 1)
+# numbers.pop(second_index)
+# numbers.pop(c1)
+# numbers.pop(d1)
+# print(numbers)
+
+# Задание 3
+
+# sales=(123000, 53684, 903745, 324679, 41578, 65748, 896754, 43521, 876590, 324560, 65789, 78900)
+
+# print (max(sales))
